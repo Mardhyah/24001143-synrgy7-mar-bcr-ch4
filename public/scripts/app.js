8cc2825ab448ce858d8bb2729f134880cb53c51e
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2024-06-03 17:48:18 +0700

    initial commit challenge 4

---
+++ public/scripts/app.js
@@ -0,0 +1,65 @@
+class App {
+  constructor() {
+    this.searchCar = document.getElementById('search-car')
+    this.carsArea = document.getElementById('view-cars')
+  }
+
+  run = () => {
+    let passengerCount = document.getElementById('passengerCount').value;
+    console.log(passengerCount);
+
+    setTimeout(() => {
+      const filtererCars = Car.list.filter(cars => cars.capacity == passengerCount);
+      console.log(filtererCars);
+
+      this.carsArea.innerHTML = '';
+
+      if (filtererCars.length === 0) {
+        this.carsArea.innerHTML = `<h2 class="justify-content-center">Data Mobil Tidak Ditemukan</h2>`;
+      } else {
+        filtererCars.map((car) => {
+          this.carsArea.innerHTML +=
+            `
+            <div class="col-md-4">
+              <div class="card" style="width: 22rem;">
+                <div class="card-body">
+                  <img src="${car.image}" class="card-img-top mb-5" alt="${car.manufacture}" height="250px">
+                  <h6 class="card-subtitle mb-4">${car.manufacture} / ${car.model} ${car.type}</h6>
+                  <h5 class="card-title">Rp ${car.rentPerDay} / Hari</h5>
+                  <p class="card-text">${car.description}</p>
+                  <div class="flex d-flex mb-2 gap-2">
+                    <img src="images/assets/fi_users.png" alt="" width="25">
+                    <p class="mb-0">${car.capacity} orang</p>
+                  </div>
+                  <div class="flex d-flex mb-2 gap-2">
+                    <img src="images/assets/fi_settings.png" alt="" width="25">
+                    <p class="mb-0">${car.transmission}</p>
+                  </div>
+                  <div class="flex d-flex mb-2 gap-2">
+                    <img src="images/assets/fi_calendar.png" alt="" width="25">
+                    <p class="mb-0">Tahun ${car.year}</p>
+                  </div>
+                  <a href="#" class="button1 mrgn-bt ">Pilih Mobil</a>
+                </div>
+              </div>
+            </div>
+          `;
+        });
+      }
+    }, 1000);
+  }
+
+  async load() {
+    const cars = await Binar.listCars()
+    console.log(cars)
+    Car.init(cars)
+  }
+
+  async init() {
+
+    await this.load()
+
+    this.searchCar.onclick = this.run
+  }
+
+}
